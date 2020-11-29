3c645e80c7951b4ae6dd532ad448b1f6e1f6259d
santhosh <68135777+santhosh456s@users.noreply.github.com>
2020-11-29 09:05:57 +0530

    change 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [paragraph, setparagraph] = React.useState(
+    "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  );
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para">{paragraph}</p>
+      <button id="click" onClick={() => setparagraph(paragraph)}></button>
     </div>
   );
 }
-
 
 export default App;
